f93d40a9371044cb335e247130ab00b0edfdcaa3
Damiao <damiaoalves74@hotmail.com>
2021-08-06 23:10:49 -0300

    Update visualizardados.php

--- visualizardados.php
+++ visualizardados.php
@@ -12,7 +12,7 @@ $dados = $mCli->listar();
     <head>
         <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
         <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
-        <title>Importar TXT</title>
+        <title>Importar arquivo txt</title>
 
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
 
@@ -60,7 +60,7 @@ $dados = $mCli->listar();
         else
         {
             ?>
-            tem nada
+          Arquivo vazio
         <?php } ?>
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
